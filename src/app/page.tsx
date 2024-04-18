f5ea407af0c40254e61fa66dcf960cde17781828
Asarel Castellanos <asarelc@gmail.com>
2024-04-18 13:36:43 -0700

    coming soon

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import ComingSoonBanner from "@/components/ComingSoonBanner";
+
+export default function Home() {
+  return (
+    <main>
+      <ComingSoonBanner />
+    </main>
+  );
+}
---
+++ src/components/ComingSoonBanner.tsx
@@ -0,0 +1,17 @@
+export default function ComingSoonBanner() {
+    return (
+        <div className="bg-white">
+            <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+                <div className="text-center">
+                    <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+                        smcgrades.com
+                    </p>
+                    <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+                        A website built on Next.js that can view historical grade distribution data from Santa Monica College.
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
